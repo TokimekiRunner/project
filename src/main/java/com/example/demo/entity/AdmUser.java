303d36b2544bd2d74211209cb53b058b3c8ac623
Huanye-Wang <13840806985@163.com>
2024-06-22 00:20:18 +0800

    fix download bug

---
+++ src/main/java/com/example/demo/entity/AdmUser.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+public class AdmUser {
+  public String Username;
+  public String Userpassword;
+  public String getUsername() {
+    return Username;
+  }
+
+  public void setUsername(String username) {
+    Username = username;
+  }
+
+  public String getUserpassword() {
+    return Userpassword;
+  }
+
+  public void setUserpassword(String userpassword) {
+    Userpassword = userpassword;
+  }
+
+
+}
---
+++ src/main/resources/static/script_l.js
@@ -0,0 +1,38 @@
+var responseData;
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    event.preventDefault();
+
+    var formData = {};
+    $(this).serializeArray().forEach(function (item) {
+      formData[item.name] = item.value;
+    });
+
+    var jsonData = JSON.stringify(formData);
+    console.log(jsonData);
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/login",
+      data: jsonData,
+      contentType: 'application/json',
+      success: function (response) {
+        // 处理成功响应
+        console.log("success");
+        console.log(response);
+        if (response) {
+          window.location.href = "http://localhost:8080/adm";
+        }
+        else {
+          $("<div class='success-message'>用户名或密码错误</div>").appendTo("body").fadeIn();
+          setTimeout(function(){
+            $(".success-message").fadeOut();
+          }, 3000);
+        }
+      },
+      error: function (error) {
+        // 处理错误响应
+        console.log("error");
+      }
+    });
+  });
+});
